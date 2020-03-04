337698d9bc1cdb0f7b8ee778e01136b65cd99d87
Joel Alcantara Burgos <alcantara0729@gmail.com>
2020-03-04 00:23:40 -0400

    Changes to adapt Add ThemeChooser.

---
+++ ConcentrationGame/Controllers/ThemeChooserViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ThemeChooserViewController.swift
+//  ConcentrationGame
+//
+//  Created by Joel Alcantara on 3/3/20.
+//  Copyright © 2020 JoelsTeam. All rights reserved.
+//
+
+import Foundation
---
+++ ConcentrationGame/Model/Dificulty.swift
@@ -0,0 +1,9 @@
+//
+//  Dificulty.swift
+//  ConcentrationGame
+//
+//  Created by Joel Alcantara on 3/3/20.
+//  Copyright © 2020 JoelsTeam. All rights reserved.
+//
+
+import Foundation
